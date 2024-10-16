f94872efc3706e296629f13856627391b71e224b
Roberta <roberta.amarto@ed3.com.br>
2024-10-15 22:40:05 -0300

    style: tailwind settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,18 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="bg-dark-[#ccc] text-dark-accentForeground p-4 rounded-md">
+          <h2 className="text-xl font-bold font-sans">Componente com Accent Color</h2>
+          <p>Este componente utiliza a cor de destaque para o fundo e o texto.</p>
+          <button className="bg-accent  text-secondary py-2 px-4 rounded">
+            Botão com cor de destaque
+          </button>
+        </div>
+        
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-dark-accent text-dark-card gap-2 hover:bg-dark-accentHover dark:hover:bg-acce text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,46 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    
     extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', 'sans-serif'],
+        mono: ['var(--font-geist-mono)', 'monospace'],
+      },
       colors: {
-        background: "hsl(120 3% 6%)", // Corrigido para um valor HSL
-        foreground: "hsl(0 0% 98%)", // Corrigido para um valor HSL
+        // Cores para o modo dark
+        dark: {
+          background: 'hsl(120, 3%, 6%)', // Background escuro
+          foreground: 'hsl(0, 0%, 98%)', // Foreground escuro
+          card: 'hsl(150, 6%, 7%)', // Cor do card
+          cardForeground: 'hsl(0, 0%, 98%)', // Cor do texto no card
+          popover: 'hsl(150, 6%, 7%)', // Cor do popover
+          popoverForeground: 'hsl(0, 0%, 98%)', // Cor do texto no popover
+          primary: 'rgb(231 233 237)', // Cor primária
+          primaryForeground: 'hsl(0, 0%, 2%)', // Cor do texto primário
+          secondary: 'hsl(135, 5%, 15%)', // Cor secundária
+          secondaryForeground: 'hsl(0, 0%, 98%)', // Cor do texto secundário
+          muted: 'hsl(130, 5%, 22%)', // Cor muted
+          mutedForeground: 'hsl(130, 5%, 49%)', // Cor do texto muted
+          accent: 'hsl(132, 40%, 88%)', // Cor de destaque
+          accentForeground: 'hsl(0, 0%, 98%)', // Cor do texto de destaque
+          accentHover: '#adc6b6', // Cor de destaque no hover
+          destructive: 'hsl(0, 84%, 60%)', // Cor de ação destrutiva
+          destructiveForeground: 'hsl(0, 0%, 98%)', // Texto destrutivo
+          border: 'hsl(140, 5%, 12%)', // Cor da borda
+          input: 'hsl(135, 4%, 18%)', // Cor de input
+          ring: 'hsl(132, 40%, 40%)', // Cor de ring
+        },
       },
     },
   },
+
   plugins: [],
 };
 
