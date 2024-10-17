12f41d7595de19064ca702572752cba89e9eb733
Roberta <roberta.amarto@ed3.com.br>
2024-10-16 22:12:13 -0300

    featÇ: update menu Mobile behaviour

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,24 +3,21 @@ import Link from 'next/link';
 import headerData from '../data/headerData.json';
 
 interface MenuProps {
-  isMobile: boolean;  // Defina o tipo da prop isMobile como boolean
-  closeMenu: () => void; // Defina o tipo da prop closeMenu como uma função sem parâmetros
+  isMobile: boolean;  // Define o tipo da prop isMobile como boolean
+  closeMenu: () => void; // Define o tipo da prop closeMenu como uma função sem parâmetros
 }
 
 const Menu: React.FC<MenuProps> = ({ isMobile, closeMenu }) => {
   return (
-    <div
-      className={`absolute inset-0 flex items-center justify-center ${isMobile ? 'block' : 'hidden'} md:hidden`}
-      onClick={closeMenu}
-    >
-      <nav className="bg-background/95 backdrop-blur-sm rounded-lg p-4">
-        <ul className="flex flex-col space-y-4">
+    <>
+      {/* Menu Desktop */}
+      <nav className={`hidden md:flex md:w-full`}>
+        <ul className="flex space-x-4">
           {headerData.links.map((item) => (
             <li key={item.url}>
               <Link
                 href={item.url}
                 className="text-secondary-foreground hover:text-primary transition-colors"
-                onClick={closeMenu} // Fecha o menu ao clicar em um link
               >
                 {item.label}
               </Link>
@@ -28,7 +25,28 @@ const Menu: React.FC<MenuProps> = ({ isMobile, closeMenu }) => {
           ))}
         </ul>
       </nav>
-    </div>
+
+      {/* Menu Mobile */}
+      {isMobile && (
+        <div className="absolute inset-0 flex items-center justify-center md:hidden" onClick={closeMenu}>
+          <nav className="flex justify-center w-full bg-background/95 backdrop-blur-sm rounded-lg p-4 pt-10">
+            <ul className="flex flex-col items-center space-y-4 w-full absolute">
+              {headerData.links.map((item) => (
+                <li key={item.url}>
+                  <Link
+                    href={item.url}
+                    className="text-secondary-foreground hover:text-primary transition-colors"
+                    onClick={closeMenu} // Fecha o menu ao clicar em um link
+                  >
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      )}
+    </>
   );
 };
 
